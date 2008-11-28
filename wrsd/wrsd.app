e23966a8d9b298a46ce012d5b672042f84a54d99
Nick Gerakines <nick@gerakines.net>
2008-11-28 15:05:23 -0800

    Cleaning up process loop interacting by adding spawn_link to supervisor tree instead of has a start_phase/3 component. Added start/stop script.

--- wrsd/wrsd.app
+++ wrsd/wrsd.app
@@ -1,15 +1,14 @@
 {application, wrsd, [
     {description, "An Erlang World of Warcraft realm status daemon"},
-    {vsn, "0.2"},
+    {vsn, "0.2.1"},
     {modules, [
         wrsd,
-        wrsd_realmserver,
+		wrsd_eurealmserver,
+		wrsd_usrealmserver,
+		wrsd_realm,
         wrsd_web
     ]},
     {registered, [wrsd]},
-    {applications, [kernel, stdlib, sasl]},
+    {applications, [kernel, stdlib, sasl, inets]},
     {mod, {wrsd, []}},
-    {start_phases, [
-        {update_loop, []}
-    ]}
 ]}.
--- wrsd/wrsd.erl
+++ wrsd/wrsd.erl
@@ -1,60 +1,33 @@
 -module(wrsd).
 -behaviour(application).
 
--export([start/2, stop/1, start_phase/3, init/1]).
--export([update_loop/0, build_rel/0]).
+-export([start/2, stop/1, init/1]).
+-export([fetch_loop/1, update_loop/0, stop/0]).
 
 start(_Type, _Args) ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-
 stop(_State) -> ok.
 
-start_phase(update_loop, _, _) ->
-    spawn(?MODULE, update_loop, []),
-    ok.
+stop() ->
+    [Node | _] = init:get_plain_arguments(),
+    Resp = rpc:call(list_to_atom(Node), init, stop, []),
+    Resp.
 
 init(Args) ->
     {ok, {{one_for_one, 2, 10}, [
         {wrsd_eurealmserver, {wrsd_eurealmserver, start_link, [Args]}, permanent, 2000, worker, [wrsd_eurealmserver]},
         {wrsd_usrealmserver, {wrsd_usrealmserver, start_link, [Args]}, permanent, 2000, worker, [wrsd_usrealmserver]},
+        {wrsd_processor, {wrsd, fetch_loop, [none]}, permanent, 2000, worker, dynamic},
         {wrsd_web, {wrsd_web, start, [nil]}, permanent, 2000, worker, dynamic}
     ]}}.
+
+fetch_loop(_) ->
+    Pid = spawn_link(?MODULE, update_loop, []),
+    {ok, Pid}.
 
 update_loop() ->
     spawn(fun() -> [wrsd_usrealmserver:write(Realm) || Realm <- wrsd_realm:process(us)] end),
     spawn(fun() -> [wrsd_eurealmserver:write(Realm) || Realm <- wrsd_realm:process(eu)] end),
     timer:sleep(60000*10),
-    wrsd:update_loop().
-
-%% @spec build_rel() -> ok
-%% @doc Creates a .rel script for the project.
-build_rel() ->
-    {ok, FD} = file:open("wrsd.rel", [write]),
-    RootDir = code:root_dir(),
-    Patterns = [
-        {RootDir ++ "/", "erts-*"},
-        {RootDir ++ "/lib/", "kernel-*"},
-        {RootDir ++ "/lib/", "stdlib-*"},
-        {RootDir ++ "/lib/", "sasl-*"},
-        {RootDir ++ "/lib/", "inets-*"}
-    ],
-    [Erts, Kerne, Stdl, Sasl, Inet] = [begin
-        [R | _ ] = filelib:wildcard(P, D),
-        [_ | [Ra] ] = string:tokens(R, "-"),
-        Ra
-    end || {D, P} <- Patterns],
-    RelInfo = {release,
-        {"wrsd", "0.2"},
-        {erts, Erts}, [
-            {kernel, Kerne},
-            {stdlib, Stdl},
-            {sasl, Sasl},
-            {inets, Inet},
-            {wrsd, "0.2"}
-        ]
-    },
-    io:format(FD, "~p.", [RelInfo]),
-    file:close(FD),
-    systools:make_script("wrsd", [local]),
-    ok.
+    update_loop().
